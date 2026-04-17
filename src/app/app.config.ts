4419c038552af8aa2d5332a18d69f393ff63439b
Lakshmi Narayana V <Narayana070203@gmail.com>
2026-04-17 22:20:11 +0530

    feat: configure application providers and implement login page UI with styling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -23,7 +23,7 @@ export const appConfig: ApplicationConfig = {
         preset: Aura,
         options: {
           prefix: 'p',
-          darkModeSelector: 'system',
+          darkModeSelector: '.dark',
           cssLayer: false
         }
       }
